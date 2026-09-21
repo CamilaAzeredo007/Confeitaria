e0c829c3507efc784053bbbe42ef3a897310efc9
CamilaAzeredo007 <camilaazeredo.bg007@academico.ifsul.edu.br>
2026-09-21 14:36:17 -0300

    config de dns

--- config/conexao.js
+++ config/conexao.js
@@ -1,6 +1,11 @@
-import mongoose from "mongoose";
 
-const url = "mongodb+srv://camilaazeredobg007_db_user:6D5Np4SjtPVEw33E@cluster0.m6jkcgr.mongodb.net/?appName=Cluster0";
+import dns from 'node:dns';
+import mongoose from 'mongoose';
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+const url = "mongodb+srv://camilaazeredobg007_db_user:6D5Np4SjtPVEw33E@cluster0.m6jkcgr.mongodb.net/";
+
 const conexao = await mongoose.connect(url);
 
 export default conexao;
